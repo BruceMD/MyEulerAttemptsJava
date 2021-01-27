03bdae7aef0bd8c68633a4c7506293078e819036
Bruce Dickie <38924242+BruceMD@users.noreply.github.com>
2021-01-27 12:01:51 +0900

    Update Problem15.java
    Bit messy. 
    
    Map() seemed to have problems using an array as a key, so I changed the int[] array to a letter combination for the numbers 0 - 19 (A-T) to store as a key.
    
    Second problem I had was I was searching in the wrong order where I should have been starting at the bottom right corner, instead I started at the top left which terrible confusion for a while. All sorted now though

--- Problem15.java
+++ Problem15.java
@@ -1,47 +1,38 @@
+import javax.swing.*;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Problem15 {
 
-    static int count = 0;
+    static long count = 0;
 
-    static Map<int[], Integer> gridCount = new HashMap<int[], Integer>();
+    static Map<String, Long> gridCount = new HashMap<String, Long>();
 
     static void lattice(){
 
-        for (int i = 0; i < 20; i++){
-            for (int j = 0; j < 20; j++){
+        for (int i = 19; i > -1; i--){
+            for (int j = 19; j > -1; j--){
                 count = 0;
-                grid(0, 0, i, j);
-                int[] tempArr = {i, j};
-                gridCount.put(tempArr, count);
-                System.out.println(gridCount.containsKey(tempArr));
-                System.out.println(tempArr[0] + " " + tempArr[1]);
-                System.out.println(gridCount.size());
+                grid(i, j, 19, 19);
+                gridCount.put(ref(i)+ref(j), count);
+//                System.out.println(ref(i) + ref(j) + " " + gridCount.get(ref(i) + ref(j)));
             }
         }
 
-        System.out.println(count);
-
-        System.out.println(gridCount.get(new int[]{19, 19}));
+        System.out.println(gridCount.get("AA"));
 
     }
 
     static void grid(int x, int y, int limx, int limy){
 
-
-
-        int[] checkArr = {x, y};
-        if (gridCount.containsKey(checkArr)){
-            System.out.println("Saved time! at " + x + y);
-            count += gridCount.get(checkArr);
+        if (gridCount.containsKey(ref(x)+ref(y))){
+            count += gridCount.get(ref(x)+ref(y));
             return;
         }
 
         if (x == limx && y == limy){
             count ++;
-            return;
         }
         else if (x == limx && y < limy){
             grid(x, y+1, limx, limy);
@@ -59,6 +50,31 @@ public class Problem15 {
                 }
             }
         }
+    }
+
+    static String ref(int num){
+
+        if (num == 0){return "A"; }
+        else if (num == 1) {return "B"; }
+        else if (num == 2) {return "C"; }
+        else if (num == 3) {return "D";}
+        else if (num == 4) {return "E";}
+        else if (num == 5) {return "F";}
+        else if (num == 6) {return "G";}
+        else if (num == 7) {return "H";}
+        else if (num == 8) {return "I";}
+        else if (num == 9) {return "J";}
+        else if (num == 10) {return "K";}
+        else if (num == 11) {return "L";}
+        else if (num == 12) {return "M";}
+        else if (num == 13) {return "N";}
+        else if (num == 14) {return "O";}
+        else if (num == 15) {return "P";}
+        else if (num == 16) {return "Q";}
+        else if (num == 17) {return "R";}
+        else if (num == 18) {return "S";}
+        else {return "T";}
+
     }
 
 }
